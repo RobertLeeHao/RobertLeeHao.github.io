a1ea7e29921c8abfd262997b905ca0885a8d6bff
RobertLeeHao <lihao245348419@126.com>
2019-06-24 23:16:37 +0800

    增加前后月移动功能

--- js/main.js
+++ js/main.js
@@ -1,48 +1,72 @@
 $(document).ready(function(){
-    var _today = new Date();
-    var _year = _today.getFullYear();
-    var _month = _today.getMonth()+1;
+    var _today = new Date(); //获取今天
+    var _year = _today.getFullYear(); //获取当月
+    var _month = _today.getMonth()+1; //获取当年
 
-    var count = 42
+    var count = 42 // 定义格子
 
-    var _firstDay = new Date(_year, _month - 1, 1);  // 当前月第一天
-    // console.log(year + "and" + month);
-    $("#calender-title").text(_year + "年" + _month + "月");
+    function getCalender(){
+       var _firstDay = new Date(_year, _month - 1, 1);  // 当前月第一天
+        // console.log(year + "and" + month);
+        $("#calender-title").text(_year + "年" + _month + "月");
 
-    for (var i = 0; i < count; i++) {
-    	var _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
-    	var _thisDayNumber = _thisDay.getDate();
-    	var _thisDayMonth = _thisDay.getMonth()+1;
-        var _liStyle = "";
-    	var _otherMonthStyle = "other";
-    	var _weekendStyle = "weekend";
-    	// if (_thisDayMonth != _month) {
-    	// 	_otherMonthStyle = "other";
-    	// }
-    	// if (_thisDay.getDay() == 6 || _thisDay.getDay() == 0) {
-    	// 	_weekendStyle = "weekend";
-    	// }
-        if (_thisDayMonth != _month) {
-            if (_thisDay.getDay() == 6 || _thisDay.getDay() == 0) {
-                _liStyle = _otherMonthStyle+ " " +_weekendStyle;
+        for (var i = 0; i < count; i++) {
+            var _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
+            var _thisDayNumber = _thisDay.getDate();
+            var _thisDayMonth = _thisDay.getMonth()+1;
+            var _liStyle = "";
+            var _otherMonthStyle = "other";
+            var _weekendStyle = "weekend";
+            if (_thisDayMonth != _month) {
+                if (_thisDay.getDay() == 6 || _thisDay.getDay() == 0) {
+                    _liStyle = _otherMonthStyle+ " " +_weekendStyle;
+                } else {
+                    _liStyle = _otherMonthStyle;
+                }
             } else {
-                _liStyle = _otherMonthStyle;
+                if (_thisDay.getDay() == 6 || _thisDay.getDay() == 0) {
+                    _liStyle = _weekendStyle;
+                } else {
+                    _liStyle = "";
+                }
             }
-        } else {
-            if (_thisDay.getDay() == 6 || _thisDay.getDay() == 0) {
-                _liStyle = _weekendStyle;
-            } else {
-                _liStyle = "";
+
+            if (_thisDay.getDate() == _today.getDate() && _thisDayMonth == _today.getMonth()+1 && _thisDay.getFullYear() == _today.getFullYear()) {
+                _liStyle += " today";
             }
+
+
+            $(".calender").append("<li class="+"'"+_liStyle+"'"+"><div class='date'>"+_thisDayNumber+"</div></li>");
         }
-
-        if (_thisDay.getDate() == _today.getDate() && _thisDayMonth == _month && _thisDay.getFullYear() == _year) {
-            _liStyle += " today";
-        }
-
-
-    	$(".calender").append("<li class="+"'"+_liStyle+"'"+"><div class='date'>"+_thisDayNumber+"</div></li>");
     }
+    function removeCalender(){
+        $(".calender *").remove();
+    }
+    getCalender();
+    // 进入上月
+    $("#lastMonth").click(function(){
+        if (_month != 1) {
+            _month -= 1;
+        } else {
+            _year -= 1;
+            _month = 12;
+        }
+        removeCalender();
+        getCalender();
+    })
+    // 进入下月
+    $("#nextMonth").click(function(){
+        if (_month != 12) {
+            _month += 1;
+        } else {
+            _year += 1;
+            _month = 1;
+        }
+        
+        removeCalender();
+        getCalender();
+    })
+
 })
 
 // calender
