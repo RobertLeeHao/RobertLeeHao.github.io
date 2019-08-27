638046033329ae6400c5b5d3b9259a548116f754
RobertLeeHao <lihao245348419@126.com>
2019-08-27 20:13:18 +0800

    修改为上传文本文件并可编辑

--- projects/weeklyReport/js/main.js
+++ projects/weeklyReport/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+	$("#input-b1").fileinput({
+		'showUpload':false, 
+		'browseOnZoneClick':true,
+		'showClose':false,
+		// 'showCaption':false,
+		// 'showUploadedThumbs':false,
+		'hideThumbnailContent':true,
+		'showBrowse':false
+	});
+	$('.file-caption-main').hide();
+	$('.editArea').hide();
+	$("#input-b1").on('change', function(event, numFiles, label) {
+		$('.file-caption-main').show();
+	    $('.editArea').show();
+	    $('.file-preview').hide();
+	});
 
+	$("#input-b1").on('fileclear', function(event) {
+	    $('.file-caption-main').hide();
+	    $('.editArea').hide();
+	    $('.file-preview').show();
+	});
+
+	$("#input-b1").on('fileloaded', function(event) {
+	    // console.log($('#input-b1').fileinput('getPreview'));
+	    var _content = $('.file-preview-text').text();
+	    $('.editArea textarea').text(_content);
+	});
 })
