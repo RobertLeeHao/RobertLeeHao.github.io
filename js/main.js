9ead1f764ed752da108c22b12fcd5ed9193c9cc6
RobertLeeHao <lihao245348419@126.com>
2019-09-04 20:26:45 +0800

    样式调整

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	// 调用窗口高度
 	$(".scetion-first, .bg, #aboutme").css("height",$(window).height());
 	$(window).resize(function () {
     	$(".scetion-first, .bg, #aboutme").css("height",$(window).height());
