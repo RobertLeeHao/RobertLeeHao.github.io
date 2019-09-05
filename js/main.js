d7b5ca1cb47c230196c44381c8e5db42321aedc7
RobertLeeHao <lihao245348419@126.com>
2019-09-05 21:25:32 +0800

    锚点缓动

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
 	$(window).resize(function () {
     	$(".scetion-first, .bg, #aboutme").css("height",$(window).height());
 	});
+	// 锚点平滑过度
+	$('a').click(function(){
+        //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
+        $('html,body').animate({scrollTop: ($($(this).attr('data-href')).offset().top -50 )},1000);
+    });
 })
