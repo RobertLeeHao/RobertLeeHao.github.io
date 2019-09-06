48c569aeafadb9eab3e7fb8d92c899a9c9d30ab7
RobertLeeHao <lihao245348419@126.com>
2019-09-06 19:51:13 +0800

    增加页面跳转

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 
 	// 调用窗口高度
-	$(".scetion-first, .bg, #aboutme").css("height",$(window).height());
+	$(".scetion-first, .bg").css("height",$(window).height());
 	$(window).resize(function () {
-    	$(".scetion-first, .bg, #aboutme").css("height",$(window).height());
+    	$(".scetion-first, .bg").css("height",$(window).height());
 	});
 	// 锚点平滑过度
 	$('a').click(function(){
         //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
-        $('html,body').animate({scrollTop: ($($(this).attr('data-href')).offset().top -50 )},1000);
+        $('html,body').animate({scrollTop: ($($(this).attr('data-href')).offset().top -50 )},500);
     });
+    // 点击跳转
+    $("#work01").click(function(){
+        window.location.href="work.html";
+    });
+    // 滚动监听
+    $(document).scroll(function() {
+        var scroH = $(document).scrollTop();  //滚动高度
+        var viewH = $(window).height();  //可见高度 
+        // var contentH = $(document).height();  //内容高度
+ 
+        if(scroH >=(viewH-50)){  //距离顶部大于100px时
+ 			console.log(scroH+"+");
+ 			$('.navbar .navbar-brand').removeClass('sr-only');
+        }
+		else {
+			if ($('.navbar .navbar-brand').hasClass('sr-only') == false) {
+				$('.navbar .navbar-brand').addClass('sr-only');
+			}
+		}
+    });
 })
