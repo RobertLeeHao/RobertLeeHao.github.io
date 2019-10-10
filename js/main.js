64423d96869970a1ac7c0987bf13942c43d488ee
RobertLeeHao <lihao245348419@126.com>
2019-10-10 17:03:48 +0800

    完成样式调整

--- js/main.js
+++ js/main.js
@@ -11,30 +11,39 @@ $(document).ready(function(){
     }
     containerWidth();
 	// 窗口高度、宽度变化时
-	$("#topsection, .bg, .artical").css("height",$(window).height());
+	$("#topsection, .artical").css("height",$(window).height());
 	$(window).resize(function () {
-    	$("#topsection, .bg, .artical").css("height",$(window).height());
+    	$("#topsection, .artical").css("height",$(window).height());
         containerWidth();
 	});
 
 	// 锚点平滑过度
-	$('.navbar-nav a').click(function(){
+	$('.navbar-nav a, .btn-up').click(function(){
 		$('body').removeClass('showartical');
         //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
         $('html,body').animate({scrollTop: ($($(this).attr('data-href')).offset().top -50 )},500);
+        if ($('.navbar-toggle').hasClass('collapsed')==false) {
+                $('.navbar-toggle').click();
+            }
     });
     // logo点击行为
     $('.navbar-brand').click(function(){
     	if($('body').hasClass('showartical')){
     		$('body').removeClass('showartical');
+            if ($('.navbar-toggle').hasClass('collapsed')==false) {
+                $('.navbar-toggle').click();
+            }
     	}
 		else {
 			//根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
         $('html,body').animate({scrollTop: ($($(this).attr('data-href')).offset().top -50 )},500);
+        if ($('.navbar-toggle').hasClass('collapsed')==false) {
+                $('.navbar-toggle').click();
+            }
 		}  
     });
     // 点击跳转
-    $(".btn-work").click(function(){
+    $(".btn-work, .btn-work-xs").click(function(){
         // 获取文章资源
         var atcNb = $(this).attr('mata-work');
         console.log(atcNb);
@@ -57,10 +66,16 @@ $(document).ready(function(){
     $(document).scroll(function() {
         var scroH = $(document).scrollTop();  //滚动高度
         var viewH = $(window).height();  //可见高度 
+        // 控制置顶按钮显示
+        if (scroH >= viewH) {
+            $(".btn-up").removeClass("hidden");
+        } else {
+            $(".btn-up").addClass("hidden");
+        }
     });
     //logos
     for (var i = 26; i >0; i--) {
-        $(".cocom").after("<div class='logo-c col-md-2'><img src='img/logos/logo"+i+".jpg'><img class='img-h' src='img/logos/logo"+i+"-h.jpg"+"'></div>");
+        $(".cocom").after("<div class='logo-c col-md-2 col-xs-4'><img src='img/logos/logo"+i+".jpg'><img class='img-h' src='img/logos/logo"+i+"-h.jpg"+"'></div>");
     }
     // $(".logo-c").hover(function(){
     //     $(this).children(".img-h").css("opacity",1);
